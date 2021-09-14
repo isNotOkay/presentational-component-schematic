4e3099f8fdbe77cc16cba06e8a1f59c01869f4d3
ko <kayodenthal@peterssoftware.de>
2021-09-14 14:02:34 +0200

    update readme // differentiate between name and path since otherwise there are issues with the generated folder structure

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 This repository is a basic Schematic implementation that serves as a starting point to create and publish Schematics to NPM.
 
+
+### Important
+
+TODO: explain why "npm link" is needed in order to call the built schematic from a Angular project
+
+### Examples
+
+```
+ng generate presentational-component:presentational-component --name demo --path shared
+```
+
 ### Testing
 
 To test locally, install `@angular-devkit/schematics-cli` globally and use the `schematics` command line tool. That tool acts the same as the `generate` command of the Angular CLI, but also has a debug mode.
--- src/presentational-component/index.ts
+++ src/presentational-component/index.ts
@@ -2,7 +2,9 @@ import { strings } from '@angular-devkit/core';
 import {
     apply,
     chain,
-    externalSchematic, MergeStrategy, mergeWith,
+    externalSchematic,
+    MergeStrategy,
+    mergeWith, move,
     Rule,
     SchematicContext,
     template,
@@ -12,19 +14,26 @@ import {
 
 export function presentationalComponent(_options: {[key: string]: any}): Rule {
     return (_tree: Tree, _context: SchematicContext) => {
+        console.log(_options);
+        // TODO: Is there a better way to locate all files into the src/app folder?
+        _options.path = `src/app/${_options.path}`;
 
         const templateSource = apply(
             url('./files'), [
                 template({
                     ..._options,
-                    classify: strings.classify,
-                    dasherize: strings.dasherize
-                })
+                    ...strings
+                }),
+                move(_options.path)
             ]
         );
 
         return chain([
-            externalSchematic('@schematics/angular', 'component', _options),
+            externalSchematic('@schematics/angular', 'component', {
+                name: _options.name,
+                path: _options.path,
+                skipImport: true
+            }),
             mergeWith(templateSource, MergeStrategy.Overwrite)
         ]);
     };
