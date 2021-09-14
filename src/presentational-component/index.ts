ae5932568e41ed3b6e93ba3c46fbcac7784d4602
ko <kayodenthal@peterssoftware.de>
2021-09-14 13:12:41 +0200

    use external "component" schematic

--- src/presentational-component/index.ts
+++ src/presentational-component/index.ts
@@ -1,27 +1,11 @@
-import { basename, strings } from '@angular-devkit/core';
-import {
-    apply,
-    chain,
-    externalSchematic,
-    MergeStrategy,
-    mergeWith,
-    Rule,
-    SchematicContext,
-    template,
-    Tree,
-    url
-} from '@angular-devkit/schematics';
+import { chain, externalSchematic, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function presentationalComponent(_options: {[key: string]: any}): Rule {
     return (_tree: Tree, _context: SchematicContext) => {
-        _options.name = basename(_options.name);
-
-        // TODO: Why is the project name "undefined"?
-        if (!_options.project) {
-            throw new Error('Please provide the project name. E.g: "--project myProject"');
-        }
+        console.log(_options);
+        /*_options.name = basename(_options.name);
 
         const templateSource = apply(
             url('./files'), [
@@ -30,13 +14,12 @@ export function presentationalComponent(_options: {[key: string]: any}): Rule {
                     classify: strings.classify,
                     dasherize: strings.dasherize
                 })
-                //move(_options.path)
             ]
-        );
+        );*/
 
         return chain([
             externalSchematic('@schematics/angular', 'component', _options),
-            mergeWith(templateSource, MergeStrategy.Overwrite)
+            //mergeWith(templateSource, MergeStrategy.Overwrite)
         ]);
     };
 }
