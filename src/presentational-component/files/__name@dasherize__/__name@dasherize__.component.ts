29dfcee86fd8dfb8a7d85025cda36d4b832e0695
ko <kayodenthal@peterssoftware.de>
2021-09-14 14:26:32 +0200

    adjust templates

--- src/presentational-component/files/__name@dasherize__/__name@dasherize__.component.ts
+++ src/presentational-component/files/__name@dasherize__/__name@dasherize__.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component, EventEmitter,
+    Input,
+    OnInit,
+    Output
+} from '@angular/core';
 
 @Component({
-    selector: 'app-<%= dasherize(name) %>-component',
-    templateUrl: './<%= dasherize(name) %>.component.html',
-    styleUrls: ['./<%= dasherize(name) %>.component.scss'],
+    selector: 'app-<%= classify(name) %>-component',
+    templateUrl: './<%= classify(name) %>.component.html',
+    styleUrls: ['./<%= classify(name) %>.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class <%= classify(name) %>Component implements OnInit, OnDestroy {
-private readonly subscription: Subscription = new Subscription();
+export class <%= classify(name) %>Component implements OnInit {
+    @Input() public exampleInput: string;
+    @Output() public exampleOutput: EventEmitter<string> = new EventEmitter<string>();
 
-    constructor() { }
-
-    ngOnInit(): void {
+    constructor() {
     }
 
-    ngOnDestroy(): void {
-        this.subscription.unsubscribe();
+    public ngOnInit(): void {
     }
 }
+
+
+
